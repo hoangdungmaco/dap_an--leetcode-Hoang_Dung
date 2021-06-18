2abe7c54c001659766a8d9e44fd3e69174bdd4bb
hoangdungmaco <69211713+hoangdungmaco@users.noreply.github.com>
2021-06-18 12:09:55 +0700

    Create 3_Longest_Substring_Without_Repeating_Characters.swift

---
+++ 3_Longest_Substring_Without_Repeating_Characters.swift
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+
+import Foundation
+
+class Solution {
+    func lengthOfLongestSubstring(_ s: String) -> Int {
+        var length = 0, chars = [Character]()
+        s.forEach({
+            if chars.contains($0) {
+                chars.removeSubrange(0...chars.firstIndex(of: $0)!)
+            }
+            chars.append($0)
+            length = max(length, chars.count)
+        })
+        return length
+    }
+}
